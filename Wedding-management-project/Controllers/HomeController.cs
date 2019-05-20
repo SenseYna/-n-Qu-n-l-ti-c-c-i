0ef888d7533bf3a57da042924c18b63863da3a4b
Dat Ho <thiendat17031999@gmail.com>
2019-05-20 15:41:13 +0700

    The First Submit of VS about UI web

---
+++ Wedding-management-project/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wedding_management_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Menu()
+        {
+            return View();
+        }
+
+        public ActionResult SoKhachHang()
+        {
+            return View();
+        }
+
+        public ActionResult PhieuKhachHang()
+        {
+            return View();
+        }
+
+        public ActionResult NhanSu()
+        {
+            return View();
+        }
+
+        public ActionResult HoaDon()
+        {
+            return View();
+        }
+
+        public ActionResult DichVu()
+        {
+            return View();
+        }
+
+        public ActionResult BaoCao()
+        {
+            return View();
+        }
+
+        public ActionResult DoanhThu()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
