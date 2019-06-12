eda417091ed94bf3c6be3aacfa288471abb1140a
Dat Ho <thiendat17031999@gmail.com>
2019-06-13 02:32:34 +0700

    22

---
+++ Wedding-management-project/Common/CommonConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wedding_management_project.Common
+{
+    public static class CommonConstants
+    {
+        public static string USER_SESSION = "USER_SESSION";
+    }
+}
---
+++ Wedding-management-project/Models/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Wedding_management_project.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Bạn cần nhập Username")]
+        [Display(Name = "Username")]
+        public string UserName { set; get; }
+
+        [Required(ErrorMessage = "Bạn cần nhập Password")]
+        [Display(Name = "Password")]
+        public string Password { set; get; }
+
+        [Display(Name = "RememberMe")]
+        public bool RememberMe { set; get; }
+       
+    }
+}
